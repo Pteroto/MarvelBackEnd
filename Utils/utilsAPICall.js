93dd5f4f3fe33e615f5b8bc284e0a04e95993d68
Gustavo Monteiro <gustavosmonteiro@outlook.com>
2015-08-23 19:01:20 -0300

    removed key

--- Utils/utilsAPICall.js
+++ Utils/utilsAPICall.js
@@ -4,8 +4,8 @@
 
 var crypto = require('crypto');
 
-var publicKey = 'cb4d52d0be976cd19824a9a3887bf177';
-var privateKey = 'a65eeb1e21b9783ac2fafabe7aec0773b2de1772';
+var publicKey = 'your public key';
+var privateKey = 'your private key';
 
 module.exports = {
 
